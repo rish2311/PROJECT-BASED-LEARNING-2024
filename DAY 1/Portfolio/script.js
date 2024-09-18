93e7aa0cde2fa4770a1e1e2d6df299a157d26de4
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2024-09-19 00:27:34 +0530

    Day 1 Project Portfolio

---
+++ DAY 1/Portfolio/script.js
@@ -0,0 +1,12 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+menuIcon.addEventListener('mouseenter ', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+})
